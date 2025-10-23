f4410e2ed1ec19abba98f85c7e7e7b81d1758cb1
Ethan Drury <rbh.ethan@icloud.com>
2025-10-23 17:16:18 +1300

    Added Detailed README

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -21,7 +21,7 @@ android {
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
         //500 Request Limit A Day
-        buildConfigField("String", "apiKey", getApiKey("apiKey"))
+        buildConfigField("String", "apiKey", "\"AIzaSyAYYSoargV0UPXdAYX_W1wXZoIwqoOlRLk\"")
     }
 
     buildTypes {
